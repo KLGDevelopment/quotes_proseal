5879b29a0206c70d65e8e1b48833ea57a0968e13
Mauricio Marchant <mauro.marchant@gmail.com>
2025-10-07 09:46:30 -0500

    fix

--- resources/views/livewire/quote-detail-master.blade.php
+++ resources/views/livewire/quote-detail-master.blade.php
@@ -22,8 +22,8 @@
                 <li class="list-group-item"><strong>Sección:</strong> {{ $this->quoteDetail->item }}</li>
                 <li class="list-group-item"><strong>Actividad:</strong> {{ $this->quoteLine->product->code }} - {{ $this->quoteLine->product->name }}</li>
                 <li class="list-group-item"><strong>Cantidad:</strong> {{ $this->quoteLine->quantity }}</li>
-                <li class="list-group-item"><strong>Valor Unitario Línea de Detalle:</strong> $ {{ number_format($this->quoteLine->getData('unit_price'), 0, ',', '.') }}</li>
-                <li class="list-group-item"><strong>Subtotal Venta Línea de Detalle:</strong> $ {{ number_format($this->quoteLine->getData('sale_value'), 0, ',', '.') }}</li>
+                <li class="list-group-item"><strong>Valor Unitario Actividad:</strong> $ {{ number_format($this->quoteLine->getData('unit_price'), 0, ',', '.') }}</li>
+                <li class="list-group-item"><strong>Subtotal Venta Actividad:</strong> $ {{ number_format($this->quoteLine->getData('sale_value'), 0, ',', '.') }}</li>
             </ul>      
         </div>
     </div>
